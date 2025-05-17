d639782bf15eef498c8163e536471966bf938a20
Enderdroyd-Z <be.srodrigues24@gmail.com>
2025-05-17 19:44:44 -0300

    add: HeaderFormFilters

--- src/components/Header/components/HeaderFormFilters.tsx
+++ src/components/Header/components/HeaderFormFilters.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import SelectorGroup from '../../Selector/SelectorGroup'
-import {falocation}
+import { FaLocationDot } from 'react-icons/fa6'
+import SelectorOption from '../../Selector/SelectorOption'
 
+const cities = [
+  { id: '1', name: 'São Paulo' },
+  { id: '2', name: 'Rio de Janeiro' },
+  { id: '3', name: 'Porto Alegre' },
+  { id: '4', name: 'Curitiba' },
+  { id: '5', name: 'Belo Horizonte' },
+]
 
 const HeaderFormFilters = () => {
   return (
     <form>
-        <SelectorGroup id='city' icon={}>
-            
+        <SelectorGroup id='city' icon={<FaLocationDot/>}>
+            <SelectorOption value='' label='Choose your city...'/>
+            {cities.map((city) => {
+              return (
+                <SelectorOption
+                  key={city.id} 
+                  value={city.name} 
+                  label={city.name}
+                />
+              )
+            })}
         </SelectorGroup>
     </form>
   )
