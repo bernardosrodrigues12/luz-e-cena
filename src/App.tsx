f3483db119b9394739100c11e927e0b1af965d05
Enderdroyd-Z <be.srodrigues24@gmail.com>
2025-05-17 18:14:23 -0300

    Inital Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Banner from "./components/Banner";
+import Link from "./components";
+
+function App() {
+  return (
+    <>
+    <Banner src="../public/assets/images/Banner Desktop.png" alt="Banner"/>
+    <Link  href="/" target="_blank">Link</Link>
+    </>
+)
+}
+
+export default App;
---
+++ src/components/Header/components/HeaderFormFilters.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SelectorGroup from '../../Selector/SelectorGroup'
+import {falocation}
+
+
+const HeaderFormFilters = () => {
+  return (
+    <form>
+        <SelectorGroup id='city' icon={}>
+            
+        </SelectorGroup>
+    </form>
+  )
+}
+
+export default HeaderFormFilters
