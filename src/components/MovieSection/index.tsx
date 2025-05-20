c2debff06658e0425104d3b50247bc77f5950806
Enderdroyd-Z <be.srodrigues24@gmail.com>
2025-05-20 15:58:40 -0300

    feat: adicionando hooks

--- src/components/MovieSection/index.tsx
+++ src/components/MovieSection/index.tsx
@@ -5,21 +5,30 @@ import InputText from '../InputText'
 import styles from './MovieSection.module.css'
 import MovieList from '../MovieList'
 import useFetchMovies from '../../hooks/useFetchMovies'
+import useFiltersMovie from '../../hooks/useFiltersMovie'
+import type React from 'react'
 
 const MovieSection = () => {
     const {movies, error, isLoading} = useFetchMovies();
+    const {searchTerm, handleSearch, setSearchTerm, filteredMovies} = useFiltersMovie(movies);
 
   return (
     <main>
         <section className={styles.container}>
             <FieldSet variant='secondary'>
-                <InputText placeholder='Buscar filmes...'/>
-                <Button variant='icon'>
+                <InputText value={searchTerm} 
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    setSearchTerm(event.target.value)} 
+                    placeholder='Buscar filmes...'
+                />
+                <Button variant='icon' onClick={handleSearch}>
                     <FaSearch />
                 </Button>
             </FieldSet>
             <h1 className={styles.title}>Em cartaz</h1>
-            <MovieList movies={movies}/>
+            {isLoading && <p>Loading Movies...</p>}
+            {error && <p className={styles.error}>{error}</p>}
+            <MovieList movies={filteredMovies}/>
         </section>
     </main>
   )
